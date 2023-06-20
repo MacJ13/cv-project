c606d10492b96715ff15a2e81def052eea5a8890
MacJ13 <jacdim02@gmail.com>
2023-06-20 21:41:25 +0200

    Update PersonalForm Component
    Create getName, getTitle, getEmail, getTel, getDescription,
    getPhotoSrc methods

--- src/components/Forms/PersonalForm.js
+++ src/components/Forms/PersonalForm.js
@@ -2,6 +2,48 @@ import React from "react";
 import Form from "./Form";
 
 class PersonalForm extends React.Component {
+  getName = (e) => {
+    const name = e.target.value;
+    const { onChangeName } = this.props;
+
+    onChangeName(name);
+  };
+
+  getTitle = (e) => {
+    const title = e.target.value;
+    const { onChangeTitle } = this.props;
+
+    onChangeTitle(title);
+  };
+
+  getEmail = (e) => {
+    const email = e.target.value;
+    const { onChangeEmail } = this.props;
+
+    onChangeEmail(email);
+  };
+
+  getTel = (e) => {
+    const tel = e.target.value;
+    const { onChangeTel } = this.props;
+
+    onChangeTel(tel);
+  };
+
+  getDescription = (e) => {
+    const desc = e.target.value;
+    const { onChangeDesc } = this.props;
+    onChangeDesc(desc);
+  };
+
+  getPhotoSrc = (e) => {
+    console.log(e.target.files[0]);
+    if (!e.target.files || !e.target.files[0]) return;
+
+    const { onChangePhoto } = this.props;
+    onChangePhoto(URL.createObjectURL(e.target.files[0]));
+  };
+
   render() {
     return (
       <Form>
