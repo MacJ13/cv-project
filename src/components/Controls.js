62700b1e9c2bbe60840062d6f351926155a198e4
MacJ13 <jacdim02@gmail.com>
2023-06-20 11:45:38 +0200

    Create Controls function component

---
+++ src/components/Controls.js
@@ -0,0 +1,17 @@
+const Controls = () => {
+  return (
+    <div className="controls">
+      <button id="btn-load" className="control-btn">
+        Load Example
+      </button>
+      <button id="btn-reset" className="control-btn">
+        Reset All
+      </button>
+      <button id="btn-pdf" className="control-btn">
+        Create PDF
+      </button>
+    </div>
+  );
+};
+
+export default Controls;
