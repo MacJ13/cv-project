5b182b0101e915bede15171832416b742b48eab2
MacJ13 <jacdim02@gmail.com>
2023-06-20 21:47:21 +0200

    Update App class component
    Create onChangeInputPhoto, onChangeInputName, onChangeInputTitle,
    onChangeInputEmail, onChangeInputTel, onChangeTextDescrition
    
    add them to PersonalForm component as props
    also create props person with personal object in PersonalForm

--- src/components/App.js
+++ src/components/App.js
@@ -27,14 +27,48 @@ class App extends React.Component {
     };
   }
 
+  onChangeInputPhoto = (photoSrc) => {
+    this.setState({ personal: { ...this.state.personal, photoSrc } });
+  };
+
+  onChangeInputName = (name) => {
+    this.setState({ personal: { ...this.state.personal, name } });
+  };
+
+  onChangeInputTitle = (title) => {
+    this.setState({ personal: { ...this.state.personal, title } });
+  };
+
+  onChangeInputEmail = (email) => {
+    this.setState({ personal: { ...this.state.personal, email } });
+  };
+
+  onChangeInputTel = (tel) => {
+    this.setState({ personal: { ...this.state.personal, tel } });
+  };
+
+  onChangeTextDescription = (description) => {
+    this.setState({ personal: { ...this.state.personal, description } });
+  };
+
   render() {
+    const { personal } = this.state;
+
     return (
       <div className="container">
         <Heading />
         <Controls />
 
         <div className="cv-form">
-          <PersonalForm />
+          <PersonalForm
+            onChangePhoto={this.onChangeInputPhoto}
+            onChangeName={this.onChangeInputName}
+            onChangeTitle={this.onChangeInputTitle}
+            onChangeEmail={this.onChangeInputEmail}
+            onChangeTel={this.onChangeInputTel}
+            onChangeDesc={this.onChangeTextDescription}
+            person={personal}
+          />
           <ExperienceForm />
           <EducationForm />
         </div>
