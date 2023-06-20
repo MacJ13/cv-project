e95575bb0e768627ee7d92aae163564e6c5f0227
MacJ13 <jacdim02@gmail.com>
2023-06-20 21:53:00 +0200

    Update render method in PersonlForm
    Create onChange event and pass proper method inside it
    and value attribute in every input element
    inside render method

--- src/components/Forms/PersonalForm.js
+++ src/components/Forms/PersonalForm.js
@@ -45,17 +45,53 @@ class PersonalForm extends React.Component {
   };
 
   render() {
+    const { name, title, email, tel, description } = this.props.person;
     return (
       <Form>
         <h3 className="form-heading">Personal Information</h3>
         <div className="form-column">
-          <input type="text" id="cv-name" placeholder="full name" />
-          <input type="text" id="cv-title" placeholder="title" />
-          <label for="cv-image" className="custom-file-input">
-            <input type="file" id="cv-image" accept=".jpg, .jpeg, .png" />
+          <input
+            type="text"
+            onChange={this.getName}
+            id="cv-name"
+            placeholder="full name"
+            value={name}
+          />
+          <input
+            type="text"
+            onChange={this.getTitle}
+            id="cv-title"
+            placeholder="title"
+            value={title}
+          />
+          <label htmlFor="cv-photo" className="custom-file-input">
+            <input
+              type="file"
+              onChange={this.getPhotoSrc}
+              id="cv-photo"
+              accept=".jpg, .jpeg, .png"
+            />
           </label>
-          <input type="tel" id="cv-tel" placeholder="phone number" />
-          <textarea id="cv-desc" placeholder="description" />
+          <input
+            type="email"
+            onChange={this.getEmail}
+            id="cv-email"
+            placeholder="email"
+            value={email}
+          />
+          <input
+            type="tel"
+            onChange={this.getTel}
+            id="cv-tel"
+            placeholder="phone number"
+            value={tel}
+          />
+          <textarea
+            id="cv-description"
+            onChange={this.getDescription}
+            placeholder="description"
+            value={description}
+          />
         </div>
       </Form>
     );
