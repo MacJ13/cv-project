ddc36bbc1117db94fc13859b24ad5132f7c6cead
MacJ13 <jacdim02@gmail.com>
2023-06-20 20:38:20 +0200

    Update App component
    Import PersonForm, ExperienceForm, EducationForm, Resume
    components
    
    Add them to render function App component

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,25 @@ import React from "react";
 import Heading from "./Heading";
 import Controls from "./Controls";
 
+import Resume from "./Resume/Resume";
+
+import PersonalForm from "./Forms/PersonalForm";
+import ExperienceForm from "./Forms/ExperienceForm";
+import EducationForm from "./Forms/EducationForm";
+
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <Heading />
         <Controls />
+
+        <div className="cv-form">
+          <PersonalForm />
+          <ExperienceForm />
+          <EducationForm />
+        </div>
+        <Resume />
       </div>
     );
   }
