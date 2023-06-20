ca466dcc77956e85f148eb1b4fcaf187253039c3
MacJ13 <jacdim02@gmail.com>
2023-06-20 20:04:01 +0200

    Create examplePersonalData object in config.js

---
+++ src/config.js
@@ -0,0 +1,12 @@
+import exampleImg from "./assets/example.jpg";
+
+const examplePersonalData = {
+  name: "Waclaw Jezioranski",
+  title: "Senior Java Developer",
+  photoSrc: exampleImg,
+  email: "example@gmail.com",
+  tel: "+48 123 456 789",
+  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod felis eu egestas feugiat. Nullam semper maximus arcu, quis vulputate nunc fringilla eget. Integer sagittis tincidunt mi, at iaculis metus tempus sit amet.`,
+};
+
+export { examplePersonalData };
