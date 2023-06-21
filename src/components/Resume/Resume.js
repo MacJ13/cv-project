be3ad9f1850e5b42c3b5a8912e3b060a4c583f16
MacJ13 <jacdim02@gmail.com>
2023-06-21 20:34:10 +0200

    Create renderEducation in Resume component

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -24,6 +24,34 @@ class Resume extends React.Component {
     });
   }
 
+  renderEducation() {
+    const { educations } = this.props;
+
+    return educations.map((education) => {
+      return (
+        <div className="resume-timeline">
+          <div className="resume-date">
+            {education.from} {education.from ? "-" : ""} {education.to}
+          </div>
+          <div className="resume-content">
+            <div className="resume-university">
+              {education.university}
+              {education.university && education.city ? ", " : ""}
+              {education.city}
+            </div>
+            <div className="resume-degree">
+              {" "}
+              {!education.degree || `Degree: ${education.degree}`}
+            </div>
+            <div className="resume-subject">
+              {!education.subject || `Subject: ${education.subject}`}
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     const { name, title, photoSrc, email, tel, description } =
       this.props.person;
@@ -31,7 +59,7 @@ class Resume extends React.Component {
     return (
       <div className="resume-draft">
         <div className="resume-column-sm">
-          <h2 className="resume-name">{name || "Your name"}</h2>
+          <h2 className="resume-name">{name}</h2>
           <h3 className="resume-title">{title}</h3>
           <img
             className="resume-photo"
@@ -63,50 +91,10 @@ class Resume extends React.Component {
           <div className="resume-info">
             <h4 className="h4">Experience</h4>
             {this.renderWork()}
-            {/* <div className="resume-timeline">
-              <div className="resume-date">2015 - Present</div>
-              <div className="resume-content">
-                <div className="resume-position">Senior Web Developer</div>
-                <div className="resume-company">Facebook Inc., Menlo Park</div>
-              </div>
-            </div>
-            <div className="resume-timeline">
-              <div className="resume-date">2012 - 2015</div>
-              <div className="resume-content">
-                <div className="resume-position">Junior Web Developer</div>
-                <div className="resume-company">Tesla Inc., Palo Alto</div>
-              </div>
-            </div>
-            <div className="resume-timeline">
-              <div className="resume-date">2010 - 2012</div>
-              <div className="resume-content">
-                <div className="resume-position">UI / UX Designer</div>
-                <div className="resume-company">Google LLC, Mountain View</div>
-              </div>
-            </div> */}
           </div>
           <div className="resume-info">
             <h4 className="h4">Education</h4>
-            <div className="resume-timeline">
-              <div className="resume-date">2008 - 2010</div>
-              <div className="resume-content">
-                <div className="resume-university">
-                  University of Technology, Oklahoma
-                </div>
-                <div className="resume-degree">Degree: Master</div>
-                <div className="resume-subject">Subject: Science</div>
-              </div>
-            </div>
-            <div className="resume-timeline">
-              <div className="resume-date">2005 - 2008</div>
-              <div className="resume-content">
-                <div className="resume-university">
-                  University of Design Art, New York
-                </div>
-                <div className="resume-degree">Degree: Bachelor</div>
-                <div className="resume-degree">Subject: Visual Art</div>
-              </div>
-            </div>
+            {this.renderEducation()}
           </div>
         </div>
       </div>
