b788e3250e8db76751c2691cca00f8cd2ad12ebd
MacJ13 <jacdim02@gmail.com>
2023-06-21 11:32:20 +0200

    Create exampleWorks and workData objects

--- src/config.js
+++ src/config.js
@@ -9,4 +9,36 @@ const examplePersonalData = {
   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod felis eu egestas feugiat. Nullam semper maximus arcu, quis vulputate nunc fringilla eget. Integer sagittis tincidunt mi, at iaculis metus tempus sit amet.`,
 };
 
-export { examplePersonalData };
+const workData = {
+  position: "",
+  company: "",
+  city: "",
+  from: "",
+  to: "",
+};
+
+const exampleWorks = [
+  {
+    position: "Senior Web Developer",
+    company: "Facebook Inc.",
+    city: "Menlo Park",
+    from: "2015",
+    to: "Present",
+  },
+  {
+    position: "Junior Web Developer",
+    company: "Tesla Inc.",
+    city: "Palo Alto",
+    from: "2012",
+    to: "2015",
+  },
+  {
+    position: "UI / UX Designer",
+    company: "Google LLC",
+    city: "Mountain View",
+    from: "2010",
+    to: "2012",
+  },
+];
+
+export { examplePersonalData, workData, exampleWorks };
