9322c36695c975c99bfb276dd14e03724d253edd
MacJ13 <jacdim02@gmail.com>
2023-07-15 12:37:49 +0200

    Update Form component

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -1,10 +1,5 @@
-const Form = ({ title, children }) => {
-  return (
-    <form className="form">
-      <h3 className="h3">{title}</h3>
-      {children}
-    </form>
-  );
+const Form = ({ children }) => {
+  return <form className="form">{children}</form>;
 };
 
 export default Form;
