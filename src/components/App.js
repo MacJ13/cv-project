618f581c457e45674936cdb894b8f21479546fd9
MacJ13 <jacdim02@gmail.com>
2023-08-02 11:42:03 +0200

    Create footer

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import React from "react";
 
 import Heading from "./Heading";
 import Main from "./Main";
+import Footer from "./Footer";
 
 const App = () => {
   return (
     <>
       <Heading />
       <Main />
+      <Footer />
     </>
   );
 };
