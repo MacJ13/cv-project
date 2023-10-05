5b399ad0fde56daa2b54a7d6ec4f750e0136f5a8
MacJ13 <jacdim02@gmail.com>
2023-10-05 22:57:08 +0200

    create settings context

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
 import "../styles/style.css";
 
-import React from "react";
+import React, { createContext, useState } from "react";
 
 import Heading from "./Heading";
 import Main from "./Main";
 import Footer from "./Footer";
+import { INIT_SETTINGS } from "../utils/constants";
+
+export const SettingsContext = createContext(null);
 
 const App = () => {
+  const [settings, setSettings] = useState(INIT_SETTINGS);
+
   return (
     <>
-      <Heading />
-      <Main />
-      <Footer />
+      <SettingsContext.Provider value={{ settings }}>
+        <Heading />
+        <Main />
+        <Footer />
+      </SettingsContext.Provider>
     </>
   );
 };
