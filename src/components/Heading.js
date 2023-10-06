894470f6a594f92dbf244bf1055a26c1aca4482a
MacJ13 <jacdim02@gmail.com>
2023-10-06 16:22:37 +0200

    Add settings button to heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,7 +1,10 @@
+import { settingsIcon } from "./Icons/icons";
+
 const Heading = () => {
   return (
     <div className="heading">
       <h1 className="h1">Resume CV Creator</h1>
+      <button className="settings-open-btn">{settingsIcon}</button>
     </div>
   );
 };
