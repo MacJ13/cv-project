9d944ff15bc0bfc9431fd98e64de3ef1304c85af
MacJ13 <jacdim02@gmail.com>
2023-10-06 16:24:47 +0200

    Create CVResume Context

--- src/components/App.js
+++ src/components/App.js
@@ -8,17 +8,45 @@ import Footer from "./Footer";
 import { INIT_SETTINGS } from "../utils/constants";
 
 export const SettingsContext = createContext(null);
+export const CVResumeContext = createContext(null);
 
 const App = () => {
   const [settings, setSettings] = useState(INIT_SETTINGS);
+  const [openSettings, setOpenSettings] = useState(false);
+
+  const changeResumeLanguage = (language) => {
+    setSettings({ ...settings, language: language });
+  };
+
+  const changeResumeColor = (color) => {
+    setSettings({ ...settings, color });
+  };
+
+  const changeResumeLayout = (layout) => {
+    setSettings({ ...settings, layout });
+  };
+
+  const changeResumeFont = (font) => {
+    setSettings({ ...settings, fontFamily: font });
+  };
 
   return (
     <>
-      <SettingsContext.Provider value={{ settings }}>
-        <Heading />
-        <Main />
-        <Footer />
-      </SettingsContext.Provider>
+      <CVResumeContext.Provider value={{ openSettings, setOpenSettings }}>
+        <SettingsContext.Provider
+          value={{
+            settings,
+            changeResumeLanguage,
+            changeResumeColor,
+            changeResumeLayout,
+            changeResumeFont,
+          }}
+        >
+          <Heading />
+          <Main />
+          <Footer />
+        </SettingsContext.Provider>
+      </CVResumeContext.Provider>
     </>
   );
 };
