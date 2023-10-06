0b39b7ea6db8428ee94d6dcac5f11c847bee4e91
MacJ13 <jacdim02@gmail.com>
2023-10-06 21:10:35 +0200

    Add color to h4 element

--- src/components/Resume/ResumeDetails.js
+++ src/components/Resume/ResumeDetails.js
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+import { SettingsContext } from "../App";
+
 const ResumeDetails = ({ cls, title, children }) => {
+  const { settings } = useContext(SettingsContext);
+  const { color } = settings;
+
   return (
     <div className={cls}>
-      <h4 className="h4">{title}</h4>
+      <h4 style={{ color }} className="h4">
+        {title}
+      </h4>
       {children}
     </div>
   );
